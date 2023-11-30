f518e55f4f99c95c4e4b50b80e2c03672fd0f4e0
syeda Faiza <syedafaiza2610@gmail.com>
2023-11-30 09:56:10 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//template literal back ticks
+
+// // var str1 ='Faiza';
+// var str2 = "Faiza";
+// // var str3 =`Faiza`;
+// // var str4 = `Faiza 
+// //              is
+// //              your 
+// //              teacher`;
+            
+// var str1 = ` ${str2} 
+// HI 
+// My 
+// Name 
+// Is`;
+// console.log(str1)
---
+++ destructuring.js
@@ -0,0 +1,20 @@
+// var arr = [1,2,4,6,7,3,9,0,6,4,34,1,2,3,5,8]
+// var a = arr[0]
+// var b = arr[2]
+// var c = arr[3]
+// var d = arr[4]
+
+// console.log(c)
+
+// var arr1 = [1,2,4,6,7]
+// var  [a,b,c,d,e] = arr1
+// console.log(a,b,c,d,e)
+
+// var cities = ["Karachi","Lahore","Islamabad","Multan"]
+// var [City1 ,City2,City3,City4] = cities
+// console.log(City1,City2,City3,City4)
+
+var friends = [`Faiza`,`Saira`,`Saba`,`Hira`,`Sama`,`Sania`]
+var [Friend1,Friend2,Friend3,Friend4] = friends
+console.log(Friend1,Friend2,Friend3,Friend4)
+
