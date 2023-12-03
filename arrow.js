8457c41df17a1fdc2b286a2cbaeae03df969e81f
syeda Faiza <syedafaiza2610@gmail.com>
2023-12-03 10:37:42 +0500

    global-scope

---
+++ arrow.js
@@ -0,0 +1,22 @@
+// same as function
+// function check() {
+    
+// }
+// const check = () => {
+// console.log("Its my first arrow function")
+// }
+// check();
+
+// const addOne = (num) => {
+//     return num + 1
+//     }
+//     const a = addOne(10)
+//     console.log(a)
+
+//     const add1 = (num) => num + 1 // implisit 
+        
+//         const b = add1(10)
+//         console.log(b)
+        // const addOne = (num) =>  num * num 
+        //     const a = addOne(6)
+        //     console.log(a)
---
+++ local-global-scope.js
@@ -0,0 +1,86 @@
+//js from begineer to professional pg 155-161
+// var num = 1
+// var num ='Faiza'
+// lat a;//c an not re-declare
+// var a = 10
+// var b = 20
+// var c = 30
+// if(true){
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+// }
+// var make global variable
+// let a = 10
+// let b = 20
+// const c = 30
+// e = "esd"
+// if(true){
+//     let a = 12
+//     let d = 40
+//      f = 50
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     console.log(f)
+// }
+// console.log(e)
+// console.log(d)
+// console.log(f)
+// for (var i = 1; i<=10; i++) {
+//     console.log("Jago")
+    
+// }
+// console.log(i)
+// for (let i = 1; i<=10; i++) {
+//     console.log("Jago")
+    
+// }
+// console.log(i)
+
+// function bare() {
+//     const eidi ='yes'
+//     let a = 500
+//     function choty() {
+//         let chota = 10
+//         console.log(eidi)
+//         console.log(a)
+//     }
+//     choty()
+//     console.log(chota)
+// }
+// bare()
+
+// if(true){
+//     const username = 'Faiza'
+//     if(username=== "Faiza"){
+//         const employ = "SMIT"
+// console.log(username +" "+  employ)
+//     }
+// }
+
+// function addOne(num) {
+//     return num + 1
+    
+// }
+// addOne(3); // anonmyous function
+
+// const addTwo = function (num) {
+//     return + 2
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
