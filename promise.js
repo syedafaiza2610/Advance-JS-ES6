b859bc5c3e0b6299c2744672d6c7ceac5a07f495
syeda Faiza <syedafaiza2610@gmail.com>
2023-12-28 10:52:36 +0500

    try

--- promise.js
+++ promise.js
@@ -6,7 +6,7 @@
   
 
  /* promise is made and consumed ya to complete hoga ya to fail hoga
- promise is asynchronous*/
+ promise is asynchronous a means anti*/
 //  console.log('first')
 //  setTimeout(() => console.log('second'), 2000);
 //  console.log('third')
@@ -26,7 +26,72 @@
 // // new Promise((resolve, reject) => {
 //     resolve()
 // })
-fetch('https://github.com/syedafaiza2610/Meal-App.git')
-.then(data => data.json())
-.then(data => console.log(data))
-.catch(data => console.log('not running'))
+// fetch('https://github.com/syedafaiza2610/Meal-App.git')
+// .then(data => data.json())
+// .then(data => console.log(data))
+// .catch(data => console.log('not running'))
+// let gender ='female';
+// const myPrime = new Promise((resolve, reject) => {
+//     if(gender === 'female'){
+//         resolve('sucess')
+//     }else{
+//         reject('failure')
+//     }
+// })
+
+// myPrime
+// .then((data) => console.log(data,1))
+// .then(() => console.log(2))
+// .catch((data) => console.log(data))
+
+// var a = 10;
+// function name1() {
+//     var a = 20;
+//     console.log(a)
+// }
+// console.log(name1(a))
+// console.log(a)
+
+// console.log(a)
+
+// sayHi() //hoisting
+// function sayHi() {
+//     console.log('hello')
+// }
+// for(let i=1; i <=100; i=i+10){
+//     for(let j=i; j < i+10; j++){
+//         document.write(j + " ")
+//     }
+//     document.write("<br>")
+// }
+// for(let i=1; i <=2; i++){
+//     for(let j=1; j<3; j++){
+//         document.write(i*j)
+//     }
+   
+// }
+// 11
+// 12
+// 21
+// 22
+// 1,2,2,4
+for (var i=1; i<=100; i=1+10) {
+    console.log(i);
+  }
+// for(var i=1;i<=2;i++){
+//     for(var j=1 ; j<=3;j++){
+//         document.write(i*j)
+//     }
+// }
+// 11 =1
+// 12 2
+// 13 3
+// 21 2
+// 22 4
+// 23 6
+
+
+
+// var a = document.getElementById('main');
+// var b= a.childNodes[1]
+// console.log(b.textContent)
